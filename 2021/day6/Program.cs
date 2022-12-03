f916af41c44209d4478e224bf1b7c17cbe060fc5
unknown <ha@hvrd.com>
2022-12-03 11:23:00 +0100

    2021 day6 initial commit

---
+++ 2021/day6/Program.cs
@@ -0,0 +1,24 @@
+﻿// https://adventofcode.com/2021/day/6
+var initial=File.ReadAllLines("input.txt").First().Split(',').Select(s=>int.Parse(s));
+
+Int64 Simulate(IEnumerable<int> initial, int iterations)
+{
+    // Bin the fish based on timer
+    var fish=new Int64[9];
+
+    foreach(var timer in initial)
+        fish[timer]++;
+
+    for(int day=0;day<iterations;day++)
+    {
+        Int64 births=fish[0];
+        for(int i=0;i<8;i++)
+            fish[i]=fish[i+1];
+        fish[8]=births;
+        fish[6]+=births;
+    }
+    return fish.Sum();
+}
+
+Console.WriteLine($"[Part1] Total fish: {80} iterations: {Simulate(initial,80)}");
+Console.WriteLine($"[Part2] Total fish: {256} iterations: {Simulate(initial,256)}");
