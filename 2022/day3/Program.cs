811981db55469a55f961c004d2d6019f32459a74
unknown <ha@hvrd.com>
2022-12-03 09:10:11 +0100

    Updated using Chunk

--- 2022/day3/Program.cs
+++ 2022/day3/Program.cs
@@ -1,17 +1,17 @@
 ﻿// https://adventofcode.com/2022/day/3
 var lines=File.ReadAllLines("input.txt").ToArray();
-var sackParts=lines.ToArray().Select(l=>(l.Take(l.Count()/2),l.TakeLast(l.Count()/2))).ToArray();
 
 int score=0;
-foreach(var l in sackParts){
-    var dup=l.Item1.Where(c=>l.Item2.Contains(c)).First();
+foreach(var l in lines){
+    var parts=l.Chunk(l.Trim().Count()/2);
+    var dup=parts.First().Where(c=>parts.Last().Contains(c)).First();
     score+=Score(dup);
 }
 Console.WriteLine($"Part 1 score: {score}");
 
 score=0;
-for(int i=0;i<lines.Count();i+=3){
-    var dup=lines[i].Where(c=>lines[i+1].Contains(c)).Where(c=>lines[i+2].Contains(c)).First();
+foreach(var group in lines.Chunk(3).Select(g=>g.ToArray())){
+    var dup=group[0].Where(c=>group[1].Contains(c)).Where(c=>group[2].Contains(c)).First();
     score+=Score(dup);
 }
 Console.WriteLine($"Part 2 score: {score}");
