900783f6e574bd79739f68c6dd6643b41b1bc9cd
unknown <ha@hvrd.com>
2022-12-03 09:23:57 +0100

    Cleanup using intersect

--- 2022/day3/Program.cs
+++ 2022/day3/Program.cs
@@ -1,21 +1,24 @@
 ﻿// https://adventofcode.com/2022/day/3
+using System.Diagnostics;
+
 var lines=File.ReadAllLines("input.txt").ToArray();
 
 int score=0;
 foreach(var l in lines){
     var parts=l.Chunk(l.Trim().Count()/2);
-    var dup=parts.First().Where(c=>parts.Last().Contains(c)).First();
-    score+=Score(dup);
+    var dup=parts.First().Intersect(parts.Last());
+    score+=Score(dup.First());
 }
 Console.WriteLine($"Part 1 score: {score}");
 
 score=0;
 foreach(var group in lines.Chunk(3).Select(g=>g.ToArray())){
-    var dup=group[0].Where(c=>group[1].Contains(c)).Where(c=>group[2].Contains(c)).First();
-    score+=Score(dup);
+    var dup=group[0].Intersect(group[1]).Intersect(group[2]);
+    score+=Score(dup.First());
 }
 Console.WriteLine($"Part 2 score: {score}");
 
 int Score(char c){
     return char.IsUpper(c)?c-'A'+27:c-'a'+1;
 }
+
