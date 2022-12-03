2f0078dce498f34eebf17d3d097ed42b41a1d92f
unknown <ha@hvrd.com>
2022-12-03 08:28:35 +0100

    Cleanup d3

--- 2022/day3/Program.cs
+++ 2022/day3/Program.cs
@@ -4,30 +4,18 @@ var sackParts=lines.ToArray().Select(l=>(l.Take(l.Count()/2),l.TakeLast(l.Count(
 
 int score=0;
 foreach(var l in sackParts){
-    var c1=new HashSet<char>(l.Item1);
-    char dup='!';
-    foreach(var c in l.Item2){
-        if(c1.Contains(c)){
-            dup=c;
-            break;
-        }
-    }
+    var dup=l.Item1.Where(c=>l.Item2.Contains(c)).First();
     score+=Score(dup);
 }
 Console.WriteLine($"Part 1 score: {score}");
 
 score=0;
-for(int i=0;i<lines.Count();i+=3)
-{
-    var c1=new HashSet<char>(lines[i]);
-    var c2=c1.Where(c=>new HashSet<char>(lines[i+1]).Contains(c));
-    var c3=c2.Where(c=>new HashSet<char>(lines[i+2]).Contains(c));
-    score+=Score(c3.First());
+for(int i=0;i<lines.Count();i+=3){
+    var dup=lines[i].Where(c=>lines[i+1].Contains(c)).Where(c=>lines[i+2].Contains(c)).First();
+    score+=Score(dup);
 }
 Console.WriteLine($"Part 2 score: {score}");
 
 int Score(char c){
     return char.IsUpper(c)?c-'A'+27:c-'a'+1;
 }
-
-
