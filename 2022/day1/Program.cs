d603510837229375e23d455f6ada7678e07507f5
unknown <ha@hvrd.com>
2022-12-04 19:44:48 +0100

    One liner 2022 day1

--- 2022/day1/Program.cs
+++ 2022/day1/Program.cs
@@ -1,5 +1,4 @@
-﻿var input=File.ReadAllText("input.txt").Replace("\r\n","|");
-var summedGroups=input.Split("||").Select(g=>g.Split("|").Select(s=>int.Parse(s)).Sum()).OrderByDescending(n=>n).ToArray();
-
+﻿var summedGroups=File.ReadAllText("input.txt").Replace("\r\n","|").Split("||"). // Groups
+Select(g=>g.Split("|").Select(s=>int.Parse(s)).Sum()).OrderByDescending(n=>n).ToArray(); // Sum and order
 Console.WriteLine($"Part 1:{summedGroups[0]}");
 Console.WriteLine($"Part 2: {summedGroups[0]+summedGroups[1]+summedGroups[2]}");            
