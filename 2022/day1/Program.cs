bb5529e85ef4ed91569757f2f5c77313df3b5a24
unknown <ha@hvrd.com>
2022-12-04 15:57:27 +0100

    Linq-version day1

--- 2022/day1/Program.cs
+++ 2022/day1/Program.cs
@@ -1,18 +1,5 @@
-﻿var data=File.ReadAllLines("input.txt").Select(l=>l.Trim());
-var calories=new List<int>();
+﻿var input=File.ReadAllText("input.txt").Replace("\r\n","|");
+var summedGroups=input.Split("||").Select(g=>g.Split("|").Select(s=>int.Parse(s)).Sum()).OrderByDescending(n=>n).ToArray();
 
-int sum=0;
-foreach(var l in data){
-    if(String.IsNullOrWhiteSpace(l)){
-        if(sum>0)
-            calories.Add(sum);
-        sum=0;
-        continue;
-    }
-    sum+=int.Parse(l);
-}
-if(sum>0)
-    calories.Add(sum);
-
-var ordered=calories.OrderByDescending(n=>n).ToArray();
-Console.WriteLine($"Total: {ordered[0]+ordered[1]+ordered[2]}");            
+Console.WriteLine($"Part 1:{summedGroups[0]}");
+Console.WriteLine($"Part 2: {summedGroups[0]+summedGroups[1]+summedGroups[2]}");            
