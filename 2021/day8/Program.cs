1bab83ae89857f8a667b376fab18f5974ecb5562
unknown <ha@hvrd.com>
2022-12-05 13:23:44 +0100

    2021 day8

--- 2021/day8/Program.cs
+++ 2021/day8/Program.cs
@@ -1,6 +1,6 @@
 ﻿// https://adventofcode.com/2021/day/8
 
-int count=0;
+int count=0, sum=0;
 foreach(var line in File.ReadAllLines("input.txt")){
     var parts = line.Split(" | ").Select(s=>s.Split(" "));
     var keys=parts.First().Select(s=>new string(s.OrderBy(_=>_).ToArray())).ToList();
@@ -16,31 +16,32 @@ foreach(var line in File.ReadAllLines("input.txt")){
     //2 9 -> L6 contains 4
     //4 0 -> L6 contains 1
     var decoded = new Dictionary<string,int>();
-    MoveString(keys,decoded,1,(l)=>l.First(_=>_.Length==2));
-    MoveString(keys,decoded,4,(l)=>l.First(_=>_.Length==4));
-    MoveString(keys,decoded,7,(l)=>l.First(_=>_.Length==3));
-    MoveString(keys,decoded,8,(l)=>l.First(_=>_.Length==7));
-
-    MoveString(keys,decoded,9,(l)=>l.First(_=>_.Length==6 && _.Contains(NumToCode(decoded,4))));
-    MoveString(keys,decoded,3,(l)=>l.First(_=>_.Length==5 && _.Contains(NumToCode(decoded,1))));
-    MoveString(keys,decoded,0,(l)=>l.First(_=>_.Length==6 && _.Contains(NumToCode(decoded,1))));
-    MoveString(keys,decoded,6,(l)=>l.First(_=>_.Length==6));
-    MoveString(keys,decoded,5,(l)=>l.First(s=>NumToCode(decoded,6).Contains(s)));
-    MoveString(keys,decoded,2,(l)=>l.First());
-
-
-    
-   // count+=values.Count(v=>uniqueLengths.Contains(v.Length));    
+    Move(keys,decoded,1,keys.First(s=>s.Length==2));
+    Move(keys,decoded,4,keys.First(s=>s.Length==4));
+    Move(keys,decoded,7,keys.First(s=>s.Length==3));
+    Move(keys,decoded,8,keys.First(s=>s.Length==7));
+    Move(keys,decoded,9,keys.First(s=>s.Length==6 && Contains(s, NumToCode(decoded,4))));
+    Move(keys,decoded,3,keys.First(s=>s.Length==5 && Contains(s, NumToCode(decoded,1))));
+    Move(keys,decoded,0,keys.First(s=>s.Length==6 && Contains(s, NumToCode(decoded,1))));
+    Move(keys,decoded,6,keys.First(_=>_.Length==6));
+    Move(keys,decoded,5,keys.First(s=>s.Length==5 && Contains(NumToCode(decoded,6), s)));
+    Move(keys,decoded,2,keys.First());
+  
+   var part1Values=new []{1,4,7,8};
+   count+=values.Count(v=> part1Values.Contains(decoded[v]));    
+   var digits = values.Select(coded=>decoded[coded]).Reverse();
+   int factor=1;
+   foreach(int digit in digits){
+        sum+=factor*digit;
+        factor*=10;
+   }
 }
-
-string NumToCode(Dictionary<string,int> decoded, int n){
-    return decoded.First(kv=>kv.Value==n).Key;
-}
-
-void MoveString(List<string> src, Dictionary<string,int> tgt, int val, Func<List<string>,string> rule){
-    var s=rule(src);
+Console.WriteLine($"Part 1: {count}");
+Console.WriteLine($"Part 2: {sum}");
+ 
+string NumToCode(Dictionary<string,int> decoded, int n) => decoded.First(kv=>kv.Value==n).Key;
+bool Contains(string container, string contained)=>contained.All(c=>container.Contains(c));
+void Move(List<string> src, Dictionary<string,int> tgt, int val, string s){
     tgt[s]=val;
     src.Remove(s);
 }
-
-Console.WriteLine($"Part 1: {count}");
