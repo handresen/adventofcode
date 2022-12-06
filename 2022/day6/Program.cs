518e394a6c871e4f2a8fbbecc33f2603cf7241d6
Håvard Andresen <ha@teleplan.no>
2022-12-06 10:00:22 +0100

    Day 6 added extra solution

--- 2022/day6/Program.cs
+++ 2022/day6/Program.cs
@@ -1,32 +1,41 @@
 ﻿// https://adventofcode.com/2022/day/6
 var s=File.ReadAllText("input.txt");
 int i;
-
-for(i=0;i<s.Length;i++)
-{
-    if(!CheckUnique(s,i,4))
+for(i=0;i<s.Length;i++){
+    if(!CheckUniqueHash(s,i,4))
         continue;
     break;
 }
 Console.WriteLine($"Part 1:{i+4}");
 
-
-for(i=0;i<s.Length;i++)
-{
-    if(!CheckUnique(s,i,14))
+for(i=0;i<s.Length;i++){
+    if(!CheckUniqueBin(s,i,14))
         continue;
     break;
 }
 Console.WriteLine($"Part 2:{i+14}");
 
-bool CheckUnique(string s, int idx, int count){
-    var ary = new int[27];
+bool CheckUniqueBin(string s, int idx, int count){
+    var bins = new int[27];
     while(count-->0){
         int c=s[idx++]-'a';
-        ary[c]++;
-        if(ary[c]>=2)
+        bins[c]++;
+        if(bins[c]>=2)
             return false;
     }
     return true;
 }
+
+bool CheckUniqueHash(string s, int idx, int count){
+    var h=new HashSet<char>();
+    while(count-->0){
+        char c=s[idx++];
+        if(h.Contains(c))
+            return false;
+        h.Add(c);
+    }
+    return true;
+}
+
+
 
