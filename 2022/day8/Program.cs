af10623c81222030076580234a7baa97a975d807
Håvard Andresen <ha@teleplan.no>
2022-12-08 12:34:57 +0100

    Cleanup day8

--- 2022/day8/Program.cs
+++ 2022/day8/Program.cs
@@ -5,7 +5,8 @@ int W=lines[0].Length;
 int H=lines.Count();
 var grid=new int[H,W];
 var dirs=new []{(-1,0),(1,0),(0,-1),(0,1)};
-bool Ok(int r, int c)=>r>=0 && c>=0 && r<H && c<W;
+bool Ok((int r, int c) coord)=>coord.r>=0 && coord.c>=0 && coord.r<H && coord.c<W;
+(int r, int c) Add((int r, int c) coord,(int dr, int dc) delta) => (coord.r+delta.dr,coord.c+delta.dc);
 
 for(int r=0;r<H;r++)
     for(int c=0;c<W;c++)
@@ -16,30 +17,30 @@ var viewScores=new List<Int64>();
 
 for(int r=0;r<H;r++){
     for(int c=0;c<W;c++){
-        visible += dirs.Any(dir=>IsVisible(r,c,dir))?1:0;
-        viewScores.Add(dirs.Select(dir=>ViewDist(r,c,dir)).Aggregate(1,(acc,val)=>acc*val));
+        visible += dirs.Any(dir=>IsVisible((r,c),dir))?1:0;
+        viewScores.Add(dirs.Select(dir=>ViewDist((r,c),dir)).Aggregate(1,(acc,val)=>acc*val));
     }
 }
 
 Console.WriteLine($"Part 1:{visible}");
 Console.WriteLine($"Part 2:{viewScores.Order().Last()}");
 
-bool IsVisible(int r, int c, (int dr, int dc) dir){
-    int val=grid[r,c];
-    r+=dir.dr;c+=dir.dc;
-    while(Ok(r,c) && grid[r,c]<val){
-        r+=dir.dr;c+=dir.dc;
+bool IsVisible((int r, int c) coord, (int dr, int dc) dir){
+    int val=grid[coord.r,coord.c];
+    coord = Add(coord,dir);
+    while(Ok(coord) && grid[coord.r,coord.c]<val){
+        coord = Add(coord,dir);
     }
-    return !Ok(r,c);    // We have reached beyond the edge
+    return !Ok(coord);    // Visible if beyond the edge
 }
 
-int ViewDist(int r, int c, (int dr, int dc) dir){
-    int val=grid[r,c];
+int ViewDist((int r, int c) coord, (int dr, int dc) dir){
+    int val=grid[coord.r,coord.c];
     int dist=0;
-    r+=dir.dr;c+=dir.dc;
-    while(Ok(r,c) && grid[r,c]<val){
+    coord=Add(coord,dir);
+    while(Ok(coord) && grid[coord.r,coord.c]<val){
         dist++;
-        r+=dir.dr;c+=dir.dc;
+        coord=Add(coord,dir);
     }
-    return dist+(Ok(r,c)?1:0);  // If we have not reached the edge, we must include last tree
+    return dist+(Ok(coord)?1:0);  // If we have not reached the edge, we must include last tree
 }
