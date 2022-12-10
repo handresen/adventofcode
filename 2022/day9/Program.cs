08061394f5e3b37f97914807b83460c197d85728
unknown <ha@hvrd.com>
2022-12-10 19:48:16 +0100

    Merge fix

--- 2022/day9/Program.cs
+++ 2022/day9/Program.cs
@@ -6,11 +6,6 @@ var move=new[]{(1,0),(0,1),(-1,0),(0,-1)};
 (int,int) Add((int x,int y) a, (int x,int y) b)=>(a.x+b.x,a.y+b.y);
 (int,int) Sub((int x,int y) a, (int x,int y) b)=>(a.x-b.x,a.y-b.y);
 (int,int) Cap((int x,int y) a) => (a.x>0?1:(a.x<0?-1:0),a.y>0?1:(a.y<0?-1:0));
-<<<<<<< HEAD
-int Len((int x,int y) a, (int x,int y) b) => (int)(Math.Sqrt((a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y)));
-(int,int) Update((int x,int y) tail, (int x,int y) head)=>
-    Len(head,tail)<=1?tail:Add(tail,Cap(Sub(head,tail)));
-=======
 int LenSq((int x,int y) a, (int x,int y) b) => ((a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y));
 
 (int,int) UpdateNext((int x,int y) tail, (int x,int y) head){
@@ -18,7 +13,6 @@ int LenSq((int x,int y) a, (int x,int y) b) => ((a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a
         return tail;
     return Add(tail,Cap(Sub(head,tail)));
 }
->>>>>>> a112878e4d9aae5cb3fb98cdd0041ac139fcae47
 
 int VisitedCount(int ropeLen){
     var rope=new (int,int)[ropeLen];
@@ -29,19 +23,11 @@ int VisitedCount(int ropeLen){
         for(int i=0;i<steps;i++){
             rope[0]=Add(rope[0],Move(dir));
             for(int j=1;j<rope.Count();j++)
-<<<<<<< HEAD
-                rope[j]=Update(rope[j],rope[j-1]);        
-=======
                 rope[j]=UpdateNext(rope[j],rope[j-1]);        
->>>>>>> a112878e4d9aae5cb3fb98cdd0041ac139fcae47
             visited.Add(rope[^1]);
         }
     }
     return visited.Count();
 }
 Console.WriteLine($"Part 1:{VisitedCount(2)}");
-<<<<<<< HEAD
 Console.WriteLine($"Part 2:{VisitedCount(10)}");
-=======
-Console.WriteLine($"Part 2:{VisitedCount(10)}");
->>>>>>> a112878e4d9aae5cb3fb98cdd0041ac139fcae47
